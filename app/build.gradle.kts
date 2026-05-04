58e0356a86980cbd8c21fc696d37c2e994a3d458
Kirill <kirill-sosnyuk@yandex.ru>
2026-05-04 13:19:56 +0100

    checkstyle fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,82 +1,99 @@
 plugins {
-    id("application")
-    id("com.github.ben-manes.versions") version "0.47.0"
-    id("org.sonarqube") version "7.3.0.8198"
-    id("com.gradleup.shadow") version "9.3.1"
-    kotlin("plugin.lombok") version "2.3.10"
-    id("io.freefair.lombok") version "9.1.0"
-    checkstyle
-    jacoco
+	application
+	jacoco
+	checkstyle
+	id("io.freefair.lombok") version "8.6"
+	id("org.springframework.boot") version "3.5.0"
+	id("io.spring.dependency-management") version "1.1.5"
+	id("io.sentry.jvm.gradle") version "6.1.0"
+	id("org.sonarqube") version "7.2.2.6593"
 }
 
 group = "hexlet.code"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("io.javalin:javalin:7.1.0")
-    implementation("io.javalin:javalin-rendering-jte:7.1.0")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
-    implementation("com.zaxxer:HikariCP:6.2.1")
-    implementation("com.h2database:h2:2.3.232")
-    implementation("org.postgresql:postgresql:42.7.7")
-    implementation("com.konghq:unirest-java-core:4.4.5")
-    implementation("org.jsoup:jsoup:1.18.3")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("io.javalin:javalin-testtools:7.1.0")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
+	implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+	implementation("org.mapstruct:mapstruct:1.6.0.Beta2")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.0.Beta2")
+
+	implementation("org.instancio:instancio-junit:4.8.1")
+	implementation("net.javacrumbs.json-unit:json-unit-assertj:3.3.0")
+	implementation("net.datafaker:datafaker:2.2.2")
+
+	runtimeOnly("com.h2database:h2:2.2.224")
+	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation(platform("org.junit:junit-bom:5.11.0-M2"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M2")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-application {
-    mainClass.set("hexlet.code.App")
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
 
 jacoco {
-    toolVersion = "0.8.12"
+	toolVersion = "0.8.12"
 }
 
 sonar {
-    properties {
-        property("sonar.projectKey", "KirillSosnyuk_java-project-99")
-        property("sonar.organization", "kirillsosnyuk")
-        property(
-            "sonar.coverage.jacoco.xmlReportPaths",
-            layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml")
-                .get()
-                .asFile
-                .absolutePath
-        )
-        property(
-            "sonar.junit.reportPaths",
-            layout.buildDirectory.dir("test-results/test")
-                .get()
-                .asFile
-                .absolutePath
-        )
-    }
+	properties {
+		property("sonar.projectKey", "KirillSosnyuk_java-project-99")
+		property("sonar.organization", "kirillsosnyuk")
+		property(
+			"sonar.coverage.jacoco.xmlReportPaths",
+			layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml")
+				.get()
+				.asFile
+				.absolutePath
+		)
+		property(
+			"sonar.junit.reportPaths",
+			layout.buildDirectory.dir("test-results/test")
+				.get()
+				.asFile
+				.absolutePath
+		)
+	}
 }
 
 tasks.test {
-    useJUnitPlatform()
-    finalizedBy(tasks.jacocoTestReport)
-    reports {
-        junitXml.required.set(true)
-        junitXml.outputLocation.set(layout.buildDirectory.dir("test-results/test"))
-        html.required.set(true)
-    }
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+	reports {
+		junitXml.required.set(true)
+		junitXml.outputLocation.set(layout.buildDirectory.dir("test-results/test"))
+		html.required.set(true)
+	}
 }
 
 tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml"))
-        html.required.set(true)
-    }
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(true)
+		xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/test/jacocoTestReport.xml"))
+		html.required.set(true)
+	}
 }
